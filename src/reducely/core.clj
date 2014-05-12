5736f653a2889f0653d098441ff00cf8feb6daa8
Stacey Sern <staceysern@gmail.com>
2014-05-12 15:54:13 -0400

    Broken

--- src/reducely/core.clj
+++ src/reducely/core.clj
@@ -1,17 +1,25 @@
 (ns reducely.core
   (:require [ring.adapter.jetty :as jetty]
+            [ring.middleware.params :as params]
             [hiccup.core :as h]
-            [formative.core :as f]))
+            [formative.core :as f]
+            [formative.parse :as fp]
+            [clojure.pprint :as p]))
+
 
 (def form
   {:fields [{:name :url}]
    :validations [[:required [:url]]]
-   :action "/yar"})
+   :action "/create"})
 
-(defn handler [req]
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body (h/html (f/render-form form))})
+(defn handler [{params :params}]
+  (do (p/pprint params)
+      {:status 200
+       :headers {"Content-Type" "text/html"}
+       :body (h/html (f/render-form form))}))
+
+(def app
+  (params/wrap-params handler))
 
 (defn boot []
-  (jetty/run-jetty #'handler {:port 8080 :join? false}))
+  (jetty/run-jetty app {:port 8080 :join? false}))
